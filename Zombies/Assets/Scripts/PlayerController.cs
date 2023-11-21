b4664faeb7223e12ea72f47a19183e40bbd91223
Ian Iversen-Krampitz <72539343+ianiversenkrampitz@users.noreply.github.com>
2023-11-21 00:35:36 -0800

    added some placeholders

--- Zombies/Assets/Scripts/PlayerController.cs
+++ Zombies/Assets/Scripts/PlayerController.cs
@@ -13,11 +13,18 @@ public class PlayerController : MonoBehaviour
     public int medAmmo;
     public int bigAmmo;
     public bool IsDown;
+    public bool takesDamage;
+    public bool hasWeapon1;
+    public bool hasWeapon2;
+    public bool hasWeapon3;
     Camera mainCamera;
     public GameObject hurt1;
     public GameObject hurt2;
     public GameObject hurt3;
-    public bool takesDamage;
+    public GameObject noWeapon;
+    public GameObject weapon1;
+    public GameObject weapon2;
+    public GameObject weapon3;
 
     // Start is called before the first frame update
     void Start()
@@ -97,8 +104,54 @@ public class PlayerController : MonoBehaviour
                 }
                 else if (hit.collider.tag == "Enemy")
                 {
-                    //whatever goes here should damage the enemy based on the weapon being used
-                    Debug.Log("bullet hit enemy.");
+                    //for weapon 1 
+                    if (hasWeapon1 == true)
+                    {
+                        if (smallAmmo >= 1)
+                        {
+                            //put reference to enemy health in enemy script
+                            //subtract amount from health  
+                            smallAmmo -= 1;
+                            Debug.Log("bullet hit enemy.");
+                        }
+                        else
+                        {
+                            //put weapon switch here 
+                            Debug.Log("no bullets for weapon1.");
+                        }
+                    }
+                    //for weapon 2
+                    if (hasWeapon2 == true)
+                    {
+                        if (medAmmo >= 1)
+                        {
+                            //put reference to enemy health in enemy script
+                            //put subtract amount from health  
+                            medAmmo -= 1;
+                            Debug.Log("bullet hit enemy.");
+                        }
+                        else
+                        {
+                            //put weapon switch here 
+                            Debug.Log("no bullets for weapon2.");
+                        }
+                    }
+                    //for weapon 3
+                    if (hasWeapon3 == true)
+                    {
+                        if (bigAmmo >= 1)
+                        {
+                            //put reference to enemy health in enemy script
+                            //put subtract amount from health  
+                            bigAmmo -= 1;
+                            Debug.Log("bullet hit enemy.");
+                        }
+                        else
+                        {
+                            //put weapon switch here 
+                            Debug.Log("no bullets for weapon3.");
+                        }
+                    }
                 }
             }
         }
@@ -133,17 +186,13 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(Damageable());
         }
     }
-    private void DealDamage()
-    {
-
-    }
     private void Die()
     {
-        //whatever goes here will make the enemy load the game over screen 
+        //put game over screen load, probably SceneManager.LoadScene(3); 
         Debug.Log("Player died.");
     }
     /// <summary>
-    /// gives player temp invincibility 
+    /// gives player temporary invincibility 
     /// </summary>
     /// <returns></returns>
     IEnumerator Damageable()
@@ -152,18 +201,21 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(1);
         takesDamage = true;
     }
+    /// <summary>
+    /// regens health back to full over time 
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Regen()
     {
         while (true)
         {
             if (health <= 30)
             {
+                //adds health for each frame with .3 second delay 
                 health++;
                 yield return new WaitForSeconds(.3f);
             }
-
             yield return new WaitForEndOfFrame();
-
         }
     }
 }
